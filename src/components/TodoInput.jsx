bd9331ca16b6061b9a4d2ed36ceedb2519286ab7
Abdulwaarith Zakariyya <abdulwaarithz@gmail.com>
2022-09-09 10:00:13 +0100

    Complete user interface ...........

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <div className="card card-body my-3">
+        <form>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book p-1"></i>
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize "
+              placeholder="add a todo item"
+            />
+          </div>
+        </form>
+        <button
+          type="submit"
+          className="btn btn-block btn-primary text-capitalize mt-3"
+        >
+          add item
+        </button>
+      </div>
+    );
+  }
+}
