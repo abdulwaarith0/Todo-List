6888ada529f356ba2872ba9ff98332d7a0cc7ca7
Abdulwaarith Zakariyya <abdulwaarithz@gmail.com>
2022-09-09 21:59:24 +0100

    Updated the todo input and todo list. User can now add a todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-import "./App.css"
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import uuid from "uuid";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-10 mx-auto col-md-8 mt-4">
-          <h3 className="text-capitalize text-center">Todo Input</h3>
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
+export default class App extends Component {
+  state = {
+    items: [],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
 
-          <TodoInput />
-          <TodoList />
-     
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    console.log(newItem);
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuidv4(),
+      editItem: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoList items= {this.state.items}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
